a61d4d6ce2cf586022c6ff690326af12dd10793c
oakoht <oakoht@utu.fi>
2026-03-05 20:16:43 +0200

    Initial Commit

---
+++ client.js
@@ -0,0 +1,6 @@
+var message = "Hello World!";
+console.log(message);
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
