3e492274476124754d0fca165ca0c9a941baf003
oakoht <oakoht@utu.fi>
2026-05-15 04:11:07 +0300

    Hub subscribing skeleton structure added!

--- src/main/resources/scripts/websocket.ts
+++ src/main/resources/scripts/websocket.ts
@@ -24,8 +24,12 @@ export class WebSocketDriver {
 
     this.webSocket.onmessage = (event) => {
       console.log("Message from server:", event.data);
-      const parserJson: ResponseMessage = JSON.parse(event.data);
-      console.log("TEST: ", parserJson);
+      try {
+        const parserJson: ResponseMessage = JSON.parse(event.data);
+        console.log("TEST: ", parserJson);
+      } catch (SyntaxError) {
+        console.log("Couldn't parse message!");
+      }
     };
 
     this.webSocket.onclose = (event) => {
--- src/main/scala/server/MainApp.scala
+++ src/main/scala/server/MainApp.scala
@@ -5,6 +5,14 @@ import zio.http._
 import zio.http.template2._
 import zio.http.ChannelEvent.{ExceptionCaught, Read, UserEvent, UserEventTriggered}
 import server.*
+import java.util.UUID
+import zio.json.ast.Json
+
+
+
+
+
+
 
 object MainApp extends ZIOAppDefault {
 
@@ -13,26 +21,57 @@ object MainApp extends ZIOAppDefault {
   println("Is game Started: " + gameState.isGameStarted)
   gameState.buildGameState()
 
-  val socketApp: WebSocketApp[Any] =
-    Handler.webSocket { channel =>
-      channel.receiveAll {
-        case Read(WebSocketFrame.Text(text)) =>
-          // Echo the message back or process it
-          ZIO.logInfo("ATTACK")
-          gameState.testOrdersAdd()
-          channel.send(Read(WebSocketFrame.Text(s"Server received: $text, ${gameState.testGetOrders()}")))
+  val hubLayer = ZLayer.fromZIO(Hub.unbounded[String])
 
-        case UserEventTriggered(ChannelEvent.UserEvent.HandshakeComplete) =>
-          gameState.testOrdersReset()
-          val player = gameState.addPlayer()
-          println(s"WebSocket connection established to ${player.id} with color ${player.color}!")
-          val response_json = s"""{"type": "PlayerData","id": ${player.id},"color": "${player.color}"}"""
-          channel.send(Read(WebSocketFrame.text(response_json)))
+  val socketApp: ZIO[Hub[String], Nothing, WebSocketApp[Any]] =
+    ZIO.service[Hub[String]].map { hub =>
+      Handler.webSocket { channel =>
+        ZIO.scoped {
+          for {
 
-        case _ =>
-          ZIO.unit
+            queue <- hub.subscribe
+
+            sendFiber <- queue.take
+              .map(WebSocketFrame.text)
+              .forEachZIO { frame =>
+                channel.send(Read(frame)).catchAll(_ => ZIO.unit)
+              }
+              .fork
+
+            _ <- channel.receiveAll {
+              case Read(WebSocketFrame.Text(text)) =>
+                // Echo the message back or process it
+                println("ATTACK")
+                gameState.testOrdersAdd()
+                val response_json = s"""{"type": "AttackOrder", "message": "$text, ${gameState.testGetOrders()}"}"""
+                channel.send(Read(WebSocketFrame.text(response_json)))
+
+              case UserEventTriggered(ChannelEvent.UserEvent.HandshakeComplete) =>
+                gameState.testOrdersReset()
+                val player = gameState.addPlayer()
+                println(s"WebSocket connection established to ${player.id} with color ${player.color}!")
+                val response_json = s"""{"type": "PlayerData","id": ${player.id},"color": "${player.color}"}"""
+                hub.publish("Player joined!")
+                channel.send(Read(WebSocketFrame.text(response_json)))
+
+              case Read(WebSocketFrame.Close(status, reason)) =>
+                Console.printLine("Closing channel with status: " + status + " and reason: " + reason)
+
+              case ChannelEvent.Unregistered =>
+                sendFiber.interrupt *>
+                ZIO.logInfo("Client disconnected")
+
+              case ExceptionCaught(cause) =>
+                Console.printLine(s"Channel error!: ${cause.getMessage}")
+
+              case _ =>
+                ZIO.unit
+            }
+          } yield ()
+        }
       }
     }
+
 
   //
   // Application HTML-Layout
@@ -63,25 +102,49 @@ object MainApp extends ZIOAppDefault {
   val htmlRoute =
     Method.GET / Root -> handler{(req: Request) =>
       Response.html(gamePage)}
+/*  val routes: ZIO[Hub[String], Nothing, Routes[Any, Response]] =
+  socketApp.map { app =>
+    Routes(
+      Method.GET / "ws" -> handler(app.toResponse)
+    )
+  }*/
+  val wsRoutes: ZIO[Hub[String], Nothing, Routes[Any, Response]] =
+    socketApp.map { socket =>
+      Routes (
+        Method.GET / "ws" -> handler(socket.toResponse)
+      )
+    }
 
-  val socketResponseRoute =
-      Method.GET / "ws" -> handler(socketApp.toResponse)
-
-  val app =
-    Routes(helloRoute,htmlRoute,socketResponseRoute) @@
+  val otherRoutes =
+    Routes(helloRoute,htmlRoute) @@
     Middleware.serveResources(path = Path.empty / "scripts" / "dist", resourcePrefix = "scripts/dist") @@
     Middleware.serveResources(path = Path.empty / "scripts" , resourcePrefix = "scripts") @@
     Middleware.serveResources(path = Path.empty / "styles", resourcePrefix = "styles") @@
     Middleware.debug
 
-  override def run =
+
+  val app: ZIO[Hub[String], Nothing, Routes[Any, Response]] =
+    wsRoutes.map(ws => otherRoutes ++ ws)
+
+
+
+
+
+  override val run =
     val cl = getClass.getClassLoader
     val url = cl.getResource("scripts/dist")
     val url2 = cl.getResource("scripts/dist/client.js")
 
 
-    Console.printLine(s"static folder found at: $url") *>
+
+    Console.printLine(s"static folder found at: $url")
+    app.flatMap(Server.serve).provide(
+      Server.default,
+      hubLayer
+    )
     //val testpath = Root / "scripts"
     //println("ROOT STRINGIKSI: " + script.inlineResource("scripts/dist/client.js"))
-    Server.serve(app).provide(Server.default)
+    //
+
+    //Server.serve(routes).provide(Server.default, hubLayer)
 }
